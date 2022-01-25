b1b90c08d99fa4cd8aeb97e17ab05da85f51a7f4
vishal sam <vinishakr9@gmail.com>
2022-01-25 16:12:15 +0530

    Added distance calculation from origin

---
+++ Distance.java
@@ -0,0 +1,22 @@
+package com.bridfgelabz.functions;
+
+import java.util.Scanner;
+
+import com.bridfgelabz.utility.Utility2;
+
+public class Distance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		      
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("enter the first number:");
+		int x = scanner.nextInt();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("enter the secnd number:");
+		int y = scan.nextInt();
+		 Utility2.Distance(x, y);
+		        
+		 
+		    }
+		}
